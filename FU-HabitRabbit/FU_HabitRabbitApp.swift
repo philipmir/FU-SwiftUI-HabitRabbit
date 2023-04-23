70cdb6b6dc606fa80946b4ebbeee44f195ddebf8
philipmir <philipmir@hotmail.com>
2023-04-23 16:30:52 +0200

    Initial Commit

---
+++ FU-HabitRabbit/FU_HabitRabbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FU_HabitRabbitApp.swift
+//  FU-HabitRabbit
+//
+//  Created by Philip Miranda on 2023-04-23.
+//
+
+import SwiftUI
+
+@main
+struct FU_HabitRabbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FU-HabitRabbitUITests/FU_HabitRabbitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FU_HabitRabbitUITestsLaunchTests.swift
+//  FU-HabitRabbitUITests
+//
+//  Created by Philip Miranda on 2023-04-23.
+//
+
+import XCTest
+
+final class FU_HabitRabbitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
